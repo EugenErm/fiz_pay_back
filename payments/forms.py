22cc763ba0076be3539d0437349f47ee99895c7a
Eugen <ermolov.e@selfwork.ru>
2022-05-17 18:41:47 +0300

    add upload file form

--- payments/forms.py
+++ payments/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 
 
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.csv']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
+
+
 class UploadPaymentRegisterForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(validators=[validate_file_extension])
